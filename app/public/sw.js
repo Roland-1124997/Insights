ea25f7d8df68a3a48bfd78832a2fdbed099e272d
Roland <1124997@hr.nl>
2026-02-23 23:07:34 +0100

    feat: add API caching strategies for improved response handling

--- app/public/sw.js
+++ app/public/sw.js
@@ -1,8 +1,47 @@
 import { clientsClaim } from "workbox-core";
 import { precacheAndRoute, cleanupOutdatedCaches } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate, NetworkFirst } from "workbox-strategies";
 
 self.skipWaiting();
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
+
+
+
+
+registerRoute(
+    ({ url }) => {
+        const pathSegments = url.pathname.split('/');
+        return pathSegments.length >= 3 && pathSegments[1] === 'api' && pathSegments[2] !== 'user';
+    },
+    new NetworkFirst({
+        cacheName: 'api-cache',
+        plugins: [
+            {
+                cacheableResponse: {
+                    statuses: [200],
+                }
+            }
+        ]
+    })
+);
+registerRoute(
+    ({ url }) => {
+        const pathSegments = url.pathname.split('/');
+        return pathSegments.length >= 3 && pathSegments[1] === 'api' && pathSegments[2] == 'user';
+    },
+    new NetworkFirst({
+        cacheName: 'user-api-cache',
+        plugins: [
+            {
+                cacheableResponse: {
+                    statuses: [0, 200, 401, 403],
+                }
+            }
+        ]
+    })
+);
+
 
